ffc7ac93e573928e3301712355b4cdf2838f858b
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2024-11-25 20:06:13 -0300

    Delete: Findbyemail and nombre , se dejo rut y se convirtio en optional para poder usar las exception de optionals

--- src/main/java/com/guarderia/gestion_guarderia/repository/UsuarioRepository.java
+++ src/main/java/com/guarderia/gestion_guarderia/repository/UsuarioRepository.java
@@ -3,8 +3,9 @@ package com.guarderia.gestion_guarderia.repository;
 import com.guarderia.gestion_guarderia.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
- Usuario findByEmail(String email);
- Usuario findByNombre(String username);
- Usuario findByRut(String rut);
+
+ Optional<Usuario> findByRut(String rut);
 }
