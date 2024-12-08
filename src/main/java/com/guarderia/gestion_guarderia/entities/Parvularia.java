c0e885e2ae84eb87f4988842515a020e2af9ed54
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2024-12-08 18:23:11 -0300

    Feat: Se añade especialidad

--- src/main/java/com/guarderia/gestion_guarderia/entities/Parvularia.java
+++ src/main/java/com/guarderia/gestion_guarderia/entities/Parvularia.java
@@ -1,9 +1,20 @@
 package com.guarderia.gestion_guarderia.entities;
 
 import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+
+
 public class Parvularia extends Usuario{
+    private String especialidad;
+
 
 
 }
