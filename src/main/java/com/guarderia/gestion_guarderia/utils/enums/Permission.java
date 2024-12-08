18f40d99a08730b1afdb20e45e6e2bebac795ce6
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2024-12-08 18:25:10 -0300

    Refactor: eliminar Permission no ocupados

--- src/main/java/com/guarderia/gestion_guarderia/utils/enums/Permission.java
+++ src/main/java/com/guarderia/gestion_guarderia/utils/enums/Permission.java
@@ -21,32 +21,7 @@ public enum Permission {
     ASISTENTE_PARVULO_READ("asistente_parvulo:read"),
     ASISTENTE_PARVULO_CREATE("asistente_parvulo:create"),
     ASISTENTE_PARVULO_UPDATE("asistente_parvulo:update"),
-    ASISTENTE_PARVULO_DELETE("asistente_parvulo:delete"),
-
-    // Permisos para Párvulo
-    PARVULO_READ("parvulo:read"),
-    PARVULO_CREATE("parvulo:create"),
-    PARVULO_UPDATE("parvulo:update"),
-    PARVULO_DELETE("parvulo:delete"),
-
-    // Permisos para Actividades
-    ACTIVIDAD_READ("actividad:read"),
-    ACTIVIDAD_CREATE("actividad:create"),
-    ACTIVIDAD_UPDATE("actividad:update"),
-    ACTIVIDAD_DELETE("actividad:delete"),
-
-    // Permisos para Asistencias
-    ASISTENCIA_READ("asistencia:read"),
-    ASISTENCIA_REGISTRAR("asistencia:registrar"),
-    ASISTENCIA_UPDATE("asistencia:update"),
-    ASISTENCIA_DELETE("asistencia:delete"),
-
-    // Permisos para Fotos
-    FOTO_UPLOAD("foto:upload"),
-    FOTO_DELETE("foto:delete"),
-    FOTO_READ("foto:read"),
-    FOTO_DOWNLOAD("foto:download");
-
+    ASISTENTE_PARVULO_DELETE("asistente_parvulo:delete");
     @Getter
     private final String permission;
 }
